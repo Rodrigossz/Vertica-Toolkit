928f8186b46e03361a2d29d46c09bc1b32f809a2
Rodrigo Souza <rodrigossz@gmail.com>
2017-06-16 09:25:55 -0300

    Create fast queries.sql

---
+++ fast queries.sql
@@ -0,0 +1,15 @@
+SQSQLVertica Commands
+
+select column_name,anchor_table_name,row_count from column_storage where row_count > 0 order by 3 desc;
+
+select * from system_tables order by table_name;
+
+select * from sessions;
+
+select version();
+
+select * from locks;
+
+select * from resouce_acquisitions;
+
+\! clear
